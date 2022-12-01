3524760265b31e6b0207ae612e25643f87eaf6bd
Kristian Skov <kris701kj@gmail.com>
2022-12-01 10:06:45 +0100

    Bumped .NET version and updated nuget package reference

--- BackupManager3/Views/MainView.xaml.cs
+++ BackupManager3/Views/MainView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WindowsTrayItemFramework;
+using Nanotek.WPF.WindowsTrayItemFramework;
 
 namespace BackupManager3.Views
 {
